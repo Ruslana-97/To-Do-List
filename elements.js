f6e8ad3e32104c6fc438dd88b0b02144df8bcbbc
Ruslana <panda9999999991@gmail.com>
2026-01-25 14:11:42 +0100

    Initial commit

---
+++ elements.js
@@ -0,0 +1,51 @@
+
+export const container = document.querySelector('.container');
+//Btn
+export const btnAdd = document.querySelector("[data-btnAdd]");
+export const btnOption = document.querySelector('.btnOptionen');
+export const btnCancel = document.querySelector('.btnCancel');
+
+//Input, data, textarea
+export const input = document.querySelector(".input");
+export const termin = document.querySelector('.termin');
+export const textarea = document.querySelector(".blockOption > textarea");
+export const memoryTermin = document.querySelector(".erinnernTermin");
+
+//emptytext
+export const empteList = document.querySelector(".lackOfTasks");
+//box for Tasks
+export const boxOutput = document.querySelector(".box-output");
+//pop
+export const popBlock = document.querySelector('.pop_up_addition_information');
+// arrows for option
+export const arrowRight = btnOption?.querySelector('.fa-angle-right');
+export const arrowDown = btnOption?.querySelector('.fa-chevron-down');
+// block for input text
+export const blockInputs = document.querySelector('.block-inputs');
+// block Option
+export const blockOption = document.querySelector('.blockOption');
+// title for btn Option
+export const optionenText = document.querySelector('.optionen-text');
+
+// Block with Done task
+export const boxDone = document.querySelector(".block-doneTasks");
+export const doneTitle = document.querySelector(".doneTitle");
+export const boxDoneTasks = document.querySelector(".box-done");
+export const countDoneTasks = document.querySelector(".count");
+
+// inputs Pop
+export const inputPop = document.querySelector(".inputEdit");           // <input>
+export const textAreaPop = document.querySelector(".textArea_Pop");     // <textarea>
+export const terminPop = document.querySelector(".terminPop");          // <input type="date">
+export const erinnernTerminPop = document.querySelector(".erinnernTerminPop"); // <input type="date">
+
+//Erinnerungen block
+export const erinnernData = document.querySelector('.errinern-data');
+
+//btn POP
+export const btnSpeichern = document.querySelector(".btnSpeichern");
+export const btnAbrechnen = document.querySelector(".btnAbrechnen");
+export const btnErrinerungAdd = document.querySelector('.btnErrin');
+export const btnErrinerungRemove = document.querySelector('.btnRemoveSelect');
+
+export const select = document.querySelector('select');
---
+++ script.js
@@ -0,0 +1,498 @@
+import {
+  container,
+  // Btn
+  btnAdd,
+  btnOption,
+  btnCancel,
+
+  // Input, data, textarea
+  input,
+  termin,
+  textarea,
+  memoryTermin,
+
+  // empty text
+  empteList,
+
+  // box for Tasks
+  boxOutput,
+
+  // pop
+  popBlock,
+
+  // arrows for option
+  arrowRight,
+  arrowDown,
+
+  // block for input text
+  blockInputs,
+
+  // block Option
+  blockOption,
+
+  // title for btn Option
+  optionenText,
+
+  // Block with Done task
+  boxDone,
+  doneTitle,
+  boxDoneTasks,
+  countDoneTasks,
+
+  // inputs Pop
+  inputPop,
+  textAreaPop,
+  terminPop,
+  erinnernTerminPop,
+
+  // Erinnerungen block
+  erinnernData,
+
+  // btn POP
+  btnSpeichern,
+  btnAbrechnen,
+  btnErrinerungAdd,
+  btnErrinerungRemove,
+
+  // select
+  select,
+} from "./elements.js";
+
+
+
+
+// const listDone = [];
+let activeTask = null;
+
+
+//Create new Todos
+function addTasks() {
+
+const [inputValue, infoTermin, memoryInfo, textArea] = [
+  input.value,
+  termin.value,
+  memoryTermin.value,
+  textarea.value
+];
+
+  // console.log(infoTermin);
+
+  if (inputValue.trim() == '') return
+    let str = `
+        <div class="box-tasks">
+              <div class="receivedTasks">
+                <div class="box-text-receided">
+                  <input type="checkbox" class="checkDone" />
+                  <div class="block-iput-text">
+                    <p class="textOfTask">${inputValue}</p>
+                    <div class="addition-information">
+                      <i class="fa-regular fa-calendar-days"></i>
+                      <span class="addition_data">${infoTermin}</span>
+                      <span><i class="fa-regular fa-alarm-clock"></i></span>
+                    </div>
+                    <div class="blockHiddenValue">
+                      <p class="textOfTask2">${inputValue}</p>
+                      <p class="textArea2">${textArea}</p>
+                      <p class="infoTermin">${infoTermin}</p>
+                      <p class="memoryInfo">${memoryInfo}</p>
+                      <p class="memoryType">${select.value || ''}</p>
+                    </div>
+                  </div>
+                </div>
+                <i class="fa-solid fa-ellipsis-vertical"></i>
+                <div class="addOption overflow">
+                  <button class="btnEdit">
+                    <i class="fa-regular fa-pen-to-square"></i>
+                    <p>To-do bearbeiten</p>
+                  </button>
+                  <button class="btnRemove">
+                    <i class="fa-regular fa-trash-can"></i>
+                    <p>To-do löschen</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+    `
+boxOutput.insertAdjacentHTML("beforeend", str);
+  
+  updateEmptyText();
+  
+const lastTask = boxOutput.lastElementChild;
+const addInfo = lastTask.querySelector('.addition-information');
+const calendarIcon = addInfo.querySelector('.fa-calendar-days');
+const alarmIcon = addInfo.querySelector('.fa-alarm-clock');
+const dateText = addInfo.querySelector('.addition_data');
+
+const hasDate = infoTermin !== '';
+const hasAlarm = memoryInfo !== '';
+
+const toggle = (el, show) => el.classList.toggle('overflow', !show);
+
+toggle(addInfo, hasDate || hasAlarm);
+toggle(calendarIcon, hasDate);
+toggle(dateText, hasDate);
+toggle(alarmIcon, hasAlarm);
+
+  
+//Додавання класу а саме червоний фон для календаря
+const today = new Date().toISOString().split('T')[0];
+const dateValue = infoTermin.trim(); // тут просто рядок
+if (dateValue && dateValue < today) {
+  addInfo.querySelector('.addition_data').classList.add('sofort');
+}
+
+  [input, termin, textarea, memoryTermin].forEach(el => el.value = '');
+  // if (boxOutput.children.length > 0) empteList.classList.add('none');
+   updateEmptyText();
+  // boxOutput.children.length > 0
+  //   ? empteList.classList.add('none')
+  //   : empteList.classList.remove('none');
+  const inputCheck = boxOutput.querySelectorAll(".checkDone");
+
+  inputCheck.forEach(item => {
+    item.addEventListener('input', function () {
+        const parentBox = this.closest(".box-tasks");
+        const textTask = parentBox.querySelector(".textOfTask");
+      if (this.checked) {
+        setTimeout(() => {
+          textTask.style.textDecoration = "line-through";
+        }, 400);
+        
+        setTimeout(() => {
+          boxDoneTasks.append(parentBox);
+          updateDoneBlock();
+          updateEmptyText();
+        }, 600);
+      } else {
+        setTimeout(() => {
+            textTask.style.textDecoration = "none";
+        }, 400);
+        
+        setTimeout(() => {
+          boxOutput.append(parentBox);
+          updateDoneBlock();
+          updateEmptyText();
+        }, 600);
+      }
+    });
+  });
+
+  // Після додавання нового завдання оновлюємо блок "Done"
+  updateDoneBlock();
+}
+
+function updateEmptyText() {
+   const tasksCount = boxOutput.querySelectorAll(".box-tasks").length;
+
+  tasksCount > 0
+    ? empteList.classList.add("none")
+    : empteList.classList.remove("none");
+}
+
+function updateDoneBlock() {
+  countDoneTasks.textContent = boxDoneTasks.children.length;
+
+  if (boxDoneTasks.children.length === 0) {
+    boxDone.classList.add('none');
+  } else {
+    boxDone.classList.remove('none');
+  }
+}
+
+
+
+//Add new Todos
+function editBocks(e) {
+
+  // --- MENU (три крапки)
+if (e.target.closest('.fa-ellipsis-vertical')) {
+  const task = e.target.closest('.receivedTasks');
+  const option = task.querySelector('.addOption');
+
+  const isOpen = !option.classList.contains('overflow');
+
+  // закриваємо всі
+  document
+    .querySelectorAll('.addOption')
+    .forEach(o => o.classList.add('overflow'));
+
+  // відкриваємо потрібний
+  if (!isOpen) {
+    option.classList.remove('overflow');
+  }
+
+
+const erinnernValue = task.querySelector('.memoryInfo_2');
+
+if (!erinnernData || !erinnernValue) return;
+const value = (erinnernValue.value || '').trim();
+erinnernData.classList.toggle('overflow', value === '');
+  
+// console.log(erinnernValue.value);
+  
+}
+
+  // --- DELETE
+  if (e.target.closest('.btnRemove')) {
+    const box = e.target.closest('.box-tasks');
+
+    if (box.querySelector('.receivedTasks.active')) {
+      popBlock.classList.add('overflow');
+    }
+     setTimeout(() => {
+        box.remove();
+        updateDoneBlock();
+        updateEmptyText();
+      }, 400);
+      
+    
+
+    // countDoneTasks.textContent = boxDoneTasks.children.length;
+    // if (boxDoneTasks.children.length == 1) empteList.classList.remove('none');
+    // if (boxDoneTasks.children.length === 0) boxDone.classList.add("none");
+    // if (boxOutput.children.length === 1 ) {
+    //   empteList.classList.remove('none'); 
+    // } else {
+    //   empteList.classList.add('none');
+    // }
+
+    return;
+    }
+
+  // --- EDIT
+  const btnEdit = e.target.closest('.btnEdit');
+  if (!btnEdit) return;
+
+  activeTask = btnEdit.closest('.receivedTasks');
+
+  document
+    .querySelectorAll('.receivedTasks')
+    .forEach(el => el.classList.remove('active'));
+
+  activeTask.classList.add('active');
+
+  inputPop.value =
+    activeTask.querySelector('.textOfTask2').textContent;
+
+  textAreaPop.value =
+    activeTask.querySelector('.textArea2').textContent;
+
+  terminPop.value =
+    activeTask.querySelector('.infoTermin').textContent;
+
+    const memoryText = activeTask.querySelector('.memoryInfo').textContent.trim();
+    const memoryType = activeTask.querySelector('.memoryType').textContent.trim();
+
+    // якщо є memoryInfo — показуємо блок
+    if (memoryText !== "") {
+      erinnernData.classList.remove('overflow');
+
+      select.value = memoryType || "Datum";
+
+      // якщо select = Datum => показуємо input і ставимо дату
+      if (select.value === "Datum") {
+        erinnernTerminPop.value = memoryText;
+        erinnernTerminPop.classList.remove('overflow');
+      } else {
+        // інші варіанти select
+        erinnernTerminPop.value = "";
+        erinnernTerminPop.classList.add('overflow');
+      }
+
+    } else {
+      // якщо memoryInfo пустий — ховаємо блок і очищаємо
+      erinnernData.classList.add('overflow');
+      select.value = "Datum";
+      erinnernTerminPop.value = "";
+      erinnernTerminPop.classList.add('overflow');
+    }
+
+  
+  popBlock.classList.add('open_2');
+  document
+    .querySelectorAll('.addOption')
+    .forEach(o => o.classList.add('overflow'));
+}
+
+
+//Save changes------------------------------------------------------------------!
+btnSpeichern.addEventListener('click', () => {
+  if (!activeTask) return;
+
+  if (!inputPop.checkValidity()) {
+    inputPop.reportValidity();
+    return;
+  }
+
+  activeTask.querySelector('.textOfTask2').textContent = inputPop.value;
+  activeTask.querySelector('.textOfTask').textContent = inputPop.value;
+  activeTask.querySelector('.textArea2').textContent = textAreaPop.value;
+  activeTask.querySelector('.infoTermin').textContent = terminPop.value;
+  activeTask.querySelector('.addition_data').textContent = terminPop.value;
+
+
+  // заміна document на activeTask
+const addInfo = activeTask.querySelector('.addition-information');
+const calendarIcon = addInfo.querySelector('.fa-calendar-days');
+const alarmIcon = addInfo.querySelector('.fa-alarm-clock');
+const dateText = addInfo.querySelector('.addition_data');
+
+const selectValue = select.value.trim();
+const remindValue = erinnernTerminPop.value.trim();
+
+
+  let finalValue = "";
+
+  if (selectValue === "Datum" && remindValue) {
+    finalValue = remindValue;
+  }
+  else if (selectValue && selectValue !== "Datum") {
+    finalValue = selectValue;
+  }
+
+  activeTask.querySelector('.memoryInfo').textContent = finalValue;
+  activeTask.querySelector('.memoryType').textContent = selectValue;
+
+// Видалення класу sofort якщо відбулися відповідні зміни в klendar
+const newDate = terminPop.value.trim();
+const today = new Date().toISOString().split('T')[0];
+const additionData = activeTask.querySelector('.addition_data');
+
+if (newDate && newDate < today) {
+  additionData.classList.add('sofort');
+} else {
+  additionData.classList.remove('sofort');
+}
+
+
+const hasDate = terminPop.value !== '';
+const isData = selectValue === "Datum";
+const isInputFilled = erinnernTerminPop.value.trim() !== "";
+
+const hasAlarm =
+  (selectValue !== "" && (!isData || isInputFilled)) ||
+  activeTask.querySelector('.memoryInfo').textContent.trim() !== "";
+
+const toggle = (el, show) => el.classList.toggle('overflow', !show);
+
+toggle(addInfo, hasDate || hasAlarm);
+toggle(calendarIcon, hasDate);
+toggle(dateText, hasDate);
+toggle(alarmIcon, hasAlarm);
+
+  
+  
+  popBlock.classList.remove('open_2');
+  activeTask.classList.remove('active');
+  activeTask = null;
+
+});
+
+
+  // Select
+select.addEventListener('change', () => {
+  if (select.value === "Datum") {
+    erinnernTerminPop.classList.remove('overflow');
+  } else {
+    erinnernTerminPop.classList.add('overflow');
+    erinnernTerminPop.value = ""; // очищаємо, якщо потрібно
+  }
+});
+
+
+// Btn Abrechnen
+btnAbrechnen.addEventListener('click', () => {
+  popBlock.classList.remove('open_2');
+  if (activeTask) activeTask.classList.remove('active');
+  activeTask = null;
+});
+
+// btn Errinerung add und delete
+btnErrinerungAdd.addEventListener('click', () => {
+  erinnernData.classList.remove('overflow');
+  select.value = "Datum";
+  erinnernTerminPop.value = "";
+  select.dispatchEvent(new Event('change'));
+});
+btnErrinerungRemove.addEventListener('click', () => {
+  erinnernData.classList.add('overflow');
+
+  // Ховаємо іконку
+  // document.querySelector('.fa-alarm-clock').classList.add('overflow');
+
+  // Очищаємо select та input при прихованні блоку
+  select.value = "Datum";
+  erinnernTerminPop.value = "";
+});
+ 
+
+
+//Check Validity
+function check() {
+  if (!input.checkValidity()) input.reportValidity(); 
+}
+
+//create Option
+function setOptionState(isOpen) {
+  arrowRight.classList.toggle('none', isOpen);
+  arrowDown.classList.toggle('none', !isOpen);
+
+  optionenText.textContent = isOpen ? 'Optionen ausblenden' : 'Optionen';
+  btnOption.style.width = isOpen ? '17%' : 'auto';
+
+  btnAdd.classList.toggle('overflow', isOpen);
+  blockOption.classList.toggle('active_2', isOpen);
+
+  if (isOpen) {
+    input.style.width = '85%';
+  }
+}
+//show Option
+function showOption() {
+  const isOpen = arrowRight.classList.contains('none') === false;
+  setOptionState(isOpen);
+}
+//close Option
+function closeBlock() {
+  setOptionState(false);
+}
+
+
+//Erledigte Aufgabe accordion
+function doneTasksAccordion() {
+  boxDoneTasks.classList.toggle('open');
+  boxDone.querySelector('.fa-angle-right')?.classList.toggle('none');
+  boxDone.querySelector('.fa-chevron-down')?.classList.toggle('none');
+}
+
+doneTitle.addEventListener('click', doneTasksAccordion);
+
+
+
+
+
+
+//Even btnAdd
+blockInputs.addEventListener('click', (e) => {
+  const btn = e.target.closest('.btnAdd');
+  if (!btn) return;
+  check();
+  if (input.value.trim() == '') return
+   setTimeout(() => {
+     addTasks(); 
+  }, 400);
+  
+  closeBlock();
+});
+
+
+
+//Event Enter
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') addTasks();
+});
+
+btnOption.addEventListener('click', showOption);
+btnCancel.addEventListener('click', closeBlock);
+boxOutput.addEventListener('click', editBocks);
+boxDoneTasks.addEventListener('click', editBocks);
